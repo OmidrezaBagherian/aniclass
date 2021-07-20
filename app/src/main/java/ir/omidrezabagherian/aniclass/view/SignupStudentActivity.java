2139ef48d7212753148bf80b3446f13c48dbc3e1
OmidrezaBagherian <OmidCoder21@gmail.com>
2021-07-20 20:06:33 +0430

    fix signup pages

--- app/src/main/java/ir/omidrezabagherian/aniclass/view/SignupStudentActivity.java
+++ app/src/main/java/ir/omidrezabagherian/aniclass/view/SignupStudentActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+
 import ir.omidrezabagherian.aniclass.R;
 
 import android.content.Intent;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -40,7 +42,56 @@ public class SignupStudentActivity extends AppCompatActivity {
         toolbarSignupStudent.setTitle(R.string.text_signup_student_title_toolbar);
         setSupportActionBar(toolbarSignupStudent);
 
-    }
+        buttonSignupStudentSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextSignupStudentName.getText().toString();
+                String family = editTextSignupStudentFamily.getText().toString();
+                String email = editTextSignupStudentEmail.getText().toString();
+                String nationalcode = editTextSignupStudentNationalCode.getText().toString();
+                String password = editTextSignupStudentPassword.getText().toString();
+                String confirm_password = editTextSignupStudentConfirmPassword.getText().toString();
+                if (name.length() <= 2) {
+                    Toast.makeText(SignupStudentActivity.this, R.string.text_toast_signup_student_character_name, Toast.LENGTH_SHORT).show();
+                } else {
+                    //Send email to database
+                }
+                if (family.length() <= 2) {
+                    Toast.makeText(SignupStudentActivity.this, R.string.text_toast_signup_student_character_family, Toast.LENGTH_SHORT).show();
+                } else {
+                    //Send email to database
+                }
+                if (email.equals("")) {
+                    Toast.makeText(SignupStudentActivity.this, R.string.text_toast_signup_student_empty_email, Toast.LENGTH_SHORT).show();
+                } else {
+                    if (email.equals("Omid@gmail.com")) {
+                        Toast.makeText(SignupStudentActivity.this, R.string.text_toast_signup_student_available_email, Toast.LENGTH_SHORT).show();
+                    } else {
+                        //Send email to database
+                    }
+                }
+                if (nationalcode.equals("")) {
+                    Toast.makeText(SignupStudentActivity.this, R.string.text_toast_signup_student_empty_nationalcode, Toast.LENGTH_SHORT).show();
+                } else {
+                    if (nationalcode.equals("2092561634")) {
+                        Toast.makeText(SignupStudentActivity.this, R.string.text_toast_signup_student_available_nationalcode, Toast.LENGTH_SHORT).show();
+                    } else {
+                        //Send nationalcode to database
+                    }
+                }
+                if (password.length() <= 7) {
+                    Toast.makeText(SignupStudentActivity.this, R.string.text_toast_signup_student_length_password, Toast.LENGTH_SHORT).show();
+                } else {
+                    if (confirm_password.equals(password)) {
+                        //Send password to database
+                    } else {
+                        Toast.makeText(SignupStudentActivity.this, R.string.text_toast_signup_student_confirm_password, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
+
+    }
 
 }
--- app/src/main/java/ir/omidrezabagherian/aniclass/view/SignupTeacherActivity.java
+++ app/src/main/java/ir/omidrezabagherian/aniclass/view/SignupTeacherActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+
 import ir.omidrezabagherian.aniclass.R;
 
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -43,9 +45,71 @@ public class SignupTeacherActivity extends AppCompatActivity {
         toolbarSignupTeacher.setTitle(R.string.text_signup_teacher_title_toolbar);
         setSupportActionBar(toolbarSignupTeacher);
 
+        buttonSignupTeacherCheckRegisterCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = editTextSignupTeacherRegisterCode.getText().toString();
+                if (code.equals("1234567890")) {
+                    editTextSignupTeacherName.setEnabled(true);
+                    editTextSignupTeacherFamily.setEnabled(true);
+                    editTextSignupTeacherEmail.setEnabled(true);
+                    editTextSignupTeacherNationalCode.setEnabled(true);
+                    editTextSignupTeacherPassword.setEnabled(true);
+                    editTextSignupTeacherConfirmPassword.setEnabled(true);
+                } else {
+                    Toast.makeText(SignupTeacherActivity.this, R.string.text_toast_signup_teacher_register_code, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
-
-
+        buttonSignupTeacherSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextSignupTeacherName.getText().toString();
+                String family = editTextSignupTeacherFamily.getText().toString();
+                String email = editTextSignupTeacherEmail.getText().toString();
+                String nationalcode = editTextSignupTeacherNationalCode.getText().toString();
+                String password = editTextSignupTeacherPassword.getText().toString();
+                String confirm_password = editTextSignupTeacherConfirmPassword.getText().toString();
+                if (name.length() <= 2) {
+                    Toast.makeText(SignupTeacherActivity.this, R.string.text_toast_signup_teacher_character_name, Toast.LENGTH_SHORT).show();
+                } else {
+                    //Send email to database
+                }
+                if (family.length() <= 2) {
+                    Toast.makeText(SignupTeacherActivity.this, R.string.text_toast_signup_teacher_character_family, Toast.LENGTH_SHORT).show();
+                } else {
+                    //Send email to database
+                }
+                if (email.equals("")) {
+                    Toast.makeText(SignupTeacherActivity.this, R.string.text_toast_signup_teacher_empty_email, Toast.LENGTH_SHORT).show();
+                } else {
+                    if (email.equals("Omid@gmail.com")) {
+                        Toast.makeText(SignupTeacherActivity.this, R.string.text_toast_signup_teacher_available_email, Toast.LENGTH_SHORT).show();
+                    } else {
+                        //Send email to database
+                    }
+                }
+                if (nationalcode.equals("")) {
+                    Toast.makeText(SignupTeacherActivity.this, R.string.text_toast_signup_teacher_empty_nationalcode, Toast.LENGTH_SHORT).show();
+                } else {
+                    if (nationalcode.equals("2092561634")) {
+                        Toast.makeText(SignupTeacherActivity.this, R.string.text_toast_signup_teacher_available_nationalcode, Toast.LENGTH_SHORT).show();
+                    } else {
+                        //Send nationalcode to database
+                    }
+                }
+                if (password.length() <= 7) {
+                    Toast.makeText(SignupTeacherActivity.this, R.string.text_toast_signup_teacher_length_password, Toast.LENGTH_SHORT).show();
+                } else {
+                    if (confirm_password.equals(password)) {
+                        //Send password to database
+                    } else {
+                        Toast.makeText(SignupTeacherActivity.this, R.string.text_toast_signup_teacher_confirm_password, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
     }
 
