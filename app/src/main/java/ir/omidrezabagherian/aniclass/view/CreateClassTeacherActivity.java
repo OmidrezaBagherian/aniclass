8bc12c20aa0d542298d67c717467dbc4efadd9a6
OmidrezaBagherian <OmidCoder21@gmail.com>
2021-07-22 20:55:47 +0430

    fix inputs create class

--- app/src/main/java/ir/omidrezabagherian/aniclass/view/CreateClassTeacherActivity.java
+++ app/src/main/java/ir/omidrezabagherian/aniclass/view/CreateClassTeacherActivity.java
@@ -12,9 +12,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -23,9 +27,27 @@ public class CreateClassTeacherActivity extends AppCompatActivity {
     private Toolbar toolbarCreateClassTeacher;
     private DrawerLayout drawerLayoutCreateClassTeacher;
     private NavigationView navigationViewCreateClassTeacher;
-    private EditText editTextCreateClassTeacherTitleLesson, editTextCreateClassTeacherCodeLesson, editTextCreateClassTeacherCapacity, editTextCreateClassTeacherLink,editTextCreateClassTeacherAbout;
-    private Spinner spinnerCreateClassTeacherChoiceDepartment,spinnerCreateClassTeacherChoiceUniversity;
+    private EditText editTextCreateClassTeacherTitleLesson, editTextCreateClassTeacherCodeLesson, editTextCreateClassTeacherCapacity, editTextCreateClassTeacherLink, editTextCreateClassTeacherAbout;
+    private Spinner spinnerCreateClassTeacherChoiceDepartment, spinnerCreateClassTeacherChoiceUniversity;
     private Button buttonCreateClassTeacherCreateClass;
+
+    String[] departments = {
+            "فناوری اطلاعات",
+            "علوم مهندسی",
+            "مالی و مدیریت",
+            "هنر و گرافیک",
+            "دانش و سلامت",
+            "معماری",
+            "مد و پوشاک",
+            "صنایع دستی",
+            "دروس عمومی"
+    };
+    String[] universities = {
+            "دانشگاه فنی حرفه ای پسرانه کرج",
+            "دانشگاه فنی حرفه ای دخترانه کرج"
+    };
+
+    String department, university;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +65,7 @@ public class CreateClassTeacherActivity extends AppCompatActivity {
         spinnerCreateClassTeacherChoiceUniversity = findViewById(R.id.spinner_create_class_choice_university);
         editTextCreateClassTeacherAbout = findViewById(R.id.edittext_create_class_about);
         buttonCreateClassTeacherCreateClass = findViewById(R.id.button_create_class_create_class);
+
 
         toolbarCreateClassTeacher.setTitle(R.string.text_create_class_teacher_title_toolbar);
         setSupportActionBar(toolbarCreateClassTeacher);
@@ -70,6 +93,67 @@ public class CreateClassTeacherActivity extends AppCompatActivity {
                         break;
                 }
                 return false;
+            }
+        });
+
+        ArrayAdapter arrayAdapterDepartment = new ArrayAdapter(this, android.R.layout.simple_spinner_item, departments);
+        arrayAdapterDepartment.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCreateClassTeacherChoiceDepartment.setAdapter(arrayAdapterDepartment);
+
+        spinnerCreateClassTeacherChoiceDepartment.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                department = departments[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter arrayAdapterUniversity = new ArrayAdapter(this, android.R.layout.simple_spinner_item, universities);
+        arrayAdapterUniversity.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCreateClassTeacherChoiceUniversity.setAdapter(arrayAdapterUniversity);
+
+        spinnerCreateClassTeacherChoiceUniversity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                university = universities[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        buttonCreateClassTeacherCreateClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String titleLesson = editTextCreateClassTeacherTitleLesson.getText().toString();
+                String codeLesson = editTextCreateClassTeacherCodeLesson.getText().toString();
+                String capacity = editTextCreateClassTeacherCodeLesson.getText().toString();
+                String link = editTextCreateClassTeacherLink.getText().toString();
+                String about = editTextCreateClassTeacherAbout.getText().toString();
+
+                if (titleLesson.length() <= 2) {
+                    Toast.makeText(CreateClassTeacherActivity.this, R.string.text_create_class_teacher_titleLesson, Toast.LENGTH_SHORT).show();
+                }
+                if (codeLesson.equals("")) {
+                    Toast.makeText(CreateClassTeacherActivity.this, R.string.text_create_class_teacher_codeLesson, Toast.LENGTH_SHORT).show();
+                }
+                if (capacity.equals("")) {
+                    Toast.makeText(CreateClassTeacherActivity.this, R.string.text_create_class_teacher_capacity, Toast.LENGTH_SHORT).show();
+                }
+                if (link.length() <= 10) {
+                    Toast.makeText(CreateClassTeacherActivity.this, R.string.text_create_class_teacher_link, Toast.LENGTH_SHORT).show();
+                }
+
+                Toast.makeText(CreateClassTeacherActivity.this, department, Toast.LENGTH_SHORT).show();
+                Toast.makeText(CreateClassTeacherActivity.this, university, Toast.LENGTH_SHORT).show();
+
             }
         });
 
